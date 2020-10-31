95a5e939f23ac5444f417d8c5747de4c6dd6a226
Eusebiu Biroas <eusebiu@biroas.com>
2020-10-31 22:03:06 +0200

    fix bug about requesting completing rolling pattern on login

--- media/js/login.js
+++ media/js/login.js
@@ -1,12 +1,12 @@
 //= require vendor/md5/md5.js
 
-+function() {
++function () {
 
     function onSubmit(form, callbacks) {
         var $form = $(form);
         let data = $form.serialize();
 
-        if (!tdna.patternValid) {
+        if (data.includes('pattern-1') && !tdna.patternValid) {
             swal('Woops',
                 'Please complete the writing pattern',
                 'warning');
@@ -26,17 +26,17 @@
     }
 
     function getLoginCallback($form) {
-        return function(res, text) {
-            switch(res.status) {
+        return function (res, text) {
+            switch (res.status) {
                 case 200:
                 case 201:
                     swal('Success', res.responseJSON.message, 'success');
-                    $form.hasClass('lcb-login-box-login') && $('.sweet-alert').each(function() {
+                    $form.hasClass('lcb-login-box-login') && $('.sweet-alert').each(function () {
                         $(this).find('.confirm').hide();
                         $(this).find('p').css('margin-bottom', '20px');
                     });
                     if ($form.data('refresh')) {
-                        setTimeout(function() {
+                        setTimeout(function () {
                             window.location =
                                 './' + (window.location.hash || '');
                         }, 1000);
@@ -47,23 +47,23 @@
                     break;
                 case 400:
                     swal('Woops',
-                         res.responseJSON && res.responseJSON.message,
-                         'error');
+                        res.responseJSON && res.responseJSON.message,
+                        'error');
                     break;
                 case 401:
                     swal('Woops.',
-                         'Your username or password is not correct',
-                         'warning');
+                        'Your username or password is not correct',
+                        'warning');
                     break;
                 case 403:
                     swal('Woops.',
-                         'Your account is locked',
-                         'warning');
+                        'Your account is locked',
+                        'warning');
                     break;
                 default:
                     swal('Woops.',
-                         'A server error has occured',
-                         'error');
+                        'A server error has occured',
+                        'error');
                     break;
             }
             // $indicator.removeClass('loading');
@@ -72,11 +72,11 @@
         };
     }
 
-    $(function() {
+    $(function () {
         // JVFloat
         $('input[placeholder]').jvFloat();
         // Switch between login boxes
-        $('.lcb-show-box').on('click', function() {
+        $('.lcb-show-box').on('click', function () {
             var $target = $('html').find($(this).data('target'));
             if ($target.length > 0) {
                 $target.siblings('.lcb-login-box').hide();
@@ -84,7 +84,7 @@
             }
         });
         // Show avatar
-        $('[action="./account/login"] [name="username"]').on('blur', function(e) {
+        $('[action="./account/login"] [name="username"]').on('blur', function (e) {
             var email = $(this).val();
             var valid = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(email);
             if (valid) {
@@ -96,7 +96,7 @@
             }
         });
         // Validation
-        $('form.validate').each(function() {
+        $('form.validate').each(function () {
             $(this).validate({
                 submitHandler: onSubmit
             });
